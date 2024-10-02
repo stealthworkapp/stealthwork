6f3d3e773b52be010504e5d773665f443c775d6d
Irving Jose Duran <32081816+fixmylifedesigns@users.noreply.github.com>
2024-10-03 06:29:28 +0900

    Remove glinet checker

--- src/pages/RouterDashboard.js
+++ src/pages/RouterDashboard.js
@@ -1,10 +1,9 @@
 import React from "react";
-import GLiNetConnectionChecker from "../components/GLiNetConnectionChecker";
 
 const RouterDashboard = () => {
   return (
     <div className="p-8 pt-32 bg-black text-white">
-      <GLiNetConnectionChecker />
+      coming soon
     </div>
   );
 };
